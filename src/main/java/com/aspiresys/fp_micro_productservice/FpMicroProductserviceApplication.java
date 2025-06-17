147c147b9376a4e026d98962a1c2c6aefd8cf3e9
BrunoGilRamirez <bruno.gilram@gmail.com>
2025-06-17 13:41:40 -0600

    First commit

---
+++ src/main/java/com/aspiresys/fp_micro_productservice/FpMicroProductserviceApplication.java
@@ -0,0 +1,13 @@
+package com.aspiresys.fp_micro_productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpMicroProductserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpMicroProductserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aspiresys/fp_micro_productservice/FpMicroProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspiresys.fp_micro_productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpMicroProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
